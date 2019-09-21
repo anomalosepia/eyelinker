6a00a5af96eab6c2346dedff23c57655d78bfa18
Austin Hurst <mynameisaustinhurst@gmail.com>
2019-09-21 14:46:35 -0400

    Added LICENCE file, misc cleanup

--- R/eyelinker.R
+++ R/eyelinker.R
@@ -1,4 +1,4 @@
-#' eyelinker: Import ASC Files from EyeLink Eye Trackers
+#' Import ASC Files from EyeLink Eye Trackers
 #'
 #' Dealing with unprocessed ASC files from EyeLink eye trackers can be a pain.
 #' This package aims to make importing and working with these files as fast and
@@ -10,6 +10,7 @@
 #' @name eyelinker
 NULL
 
+#' @importFrom utils tail
 #' @importFrom stringr str_sub<- str_detect fixed
 #' @importFrom tibble as_tibble add_column
 #' @importFrom readr read_lines read_tsv read_table2
